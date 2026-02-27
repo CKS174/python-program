974d3592d76b0ab1b423cfdd523b2dba93de8c5b
CKS174 <kharicaines@hotmail.com>
2026-02-27 18:48:17 +0000

    das.py

---
+++ das.py
@@ -0,0 +1,18 @@
+v =4
+w =5
+x =8
+y =2
+z =0
+z = (v+w) * x / y;
+print("value of (v=w)* x?y is" , z)
+
+
+
+name ="khari"
+age = 0
+
+
+if name == "khari" or name =="john" and age >=2 :
+    print("welcome khari.")
+else:
+    print("this password is invalid.")
